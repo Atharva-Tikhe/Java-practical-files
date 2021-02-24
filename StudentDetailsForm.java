7a8bc9e1424775dd39653b4720b496bccd446fb9
Atharva Tikhe <atharvatikhe22@gmail.com>
2021-02-24 23:49:49 +0530

    24/2/21 - finalized files
    ready to mail

--- StudentDetailsForm.java
+++ StudentDetailsForm.java
@@ -1,106 +1,114 @@
 // This is a GUI for a Students Detail Form, practical 1 of Advance Java
 
 // imports
-
-import com.sun.deploy.panel.JSmartTextArea;
-import jdk.nashorn.internal.runtime.regexp.joni.ScanEnvironment;
-
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Objects;
-import java.util.Scanner;
 
 
-public class StudentDetailsForm extends WriteToFile {
-	JFrame frame = new JFrame("Student Details Form");
-	JTabbedPane tab = new JTabbedPane();
+public class StudentDetailsForm {
+	JFrame frame = new JFrame("Student Details Form"); // main container: frame
+	JTabbedPane tab = new JTabbedPane(); // Pane to create tabs
 
-	public StudentDetailsForm() throws IOException {
-		super();
-		tab.addTab("Personal Details", new PersonalDetails());
-		tab.addTab("Education Details", new EducationDetails());
-		frame.add(tab);
-		frame.setSize(1000, 1000);
-		frame.setVisible(true);
-		frame.setLayout(null);
+
+	public StudentDetailsForm(){
+        tab.addTab("Personal Details", new PersonalDetails());
+        tab.addTab("Education Details", new EducationDetails());
+        frame.add(tab);
+	    frame.setSize(1000, 1000);
+        frame.setVisible(true);
+        frame.setLayout(null);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		
-	
-	}
+
+    }
 	
 	public static class PersonalDetails extends JPanel implements ActionListener {
-		JButton btg1 = new JButton("New");
-		JButton btg2 = new JButton("Exit ");
-		ButtonGroup btg = new ButtonGroup();
-		JRadioButton male = new JRadioButton("Male");
-		JRadioButton female = new JRadioButton("Female");
-		JLabel name = new JLabel("Name: ");
-		JLabel roll = new JLabel("Roll No. :");
-		JTextField nameT = new JTextField();
-		JTextField rollT = new JTextField();
-		JLabel Add =  new JLabel("Address :");
-		JLabel Gen =  new JLabel("Gender :");
-		JTextArea AddT = new JTextArea();
-		JTextArea infoArea = new JTextArea();
+        String inputFileName; // needed later to take file name from user
 
-		public PersonalDetails() 
+	    // personal details labels
+	    JLabel name = new JLabel("Name: ");
+        JLabel roll = new JLabel("Roll No. :");
+        JLabel add =  new JLabel("Address :");
+        JLabel gen =  new JLabel("Gender :");
+        JLabel selfIntro = new JLabel("This program is written by: \n\t Atharva Tikhe MITU19IMBI0025");
+
+        // personal details text field
+        JTextField nameT = new JTextField();
+        JTextField rollT = new JTextField();
+        JTextArea AddT = new JTextArea();
+        JTextArea infoArea = new JTextArea(); // used later; function: to display entries
+
+        // personal details gender radio box
+        ButtonGroup btg = new ButtonGroup();
+        JRadioButton male = new JRadioButton("Male");
+		JRadioButton female = new JRadioButton("Female");
+        JButton btg1 = new JButton("New");
+        JButton btg2 = new JButton("Exit ");
+
+        JScrollPane sp = new JScrollPane(AddT);
+
+
+
+        public PersonalDetails()
 		{
-			setLayout(null);
+			setLayout(null); // setting the layout to null helps in arranging components based on setBounds
+
 			name.setBounds(20, 20, 50,10);
-			nameT.setBounds(100,20, 100, 20);
+			nameT.setBounds(100,20, 200, 20);
 			nameT.setEditable(false);
 			
 			roll.setBounds(20, 60, 50, 10);
-			rollT.setBounds(100,50, 100, 20);
+			rollT.setBounds(100,50, 200, 20);
 			rollT.setEditable(false);
 			
-			Add.setBounds(20, 90, 60, 10);
-			AddT.setBounds(100,80, 200, 50);
+			add.setBounds(20, 90, 60, 10);
+			sp.setBounds(100,80, 350, 130);
 			AddT.setEditable(false);
 
-			Gen.setBounds(20, 150, 50, 10);
+			gen.setBounds(20, 220, 50, 10);
 			btg.add(male);
 			btg.add(female);
 			
-			male.setBounds(100, 150, 70,15);
-			female.setBounds(170, 150, 70,15);
+			male.setBounds(100, 220, 70,15);
+			female.setBounds(170, 220, 70,15);
 			male.setEnabled(false);
 			female.setEnabled(false);
-			add(name);
-			add(nameT);
-			add(roll);
-			add(rollT);
-			add(Add);
-			add(AddT);
-			add(Gen);
-			add(male);
-			add(female);
 
-			btg1.setBounds(100, 200, 70, 20);
+			btg1.setBounds(100, 260, 70, 20);
 			btg1.addActionListener(this);
 			add(btg1);
 			
-			btg2.setBounds(180, 200, 70, 20);
+			btg2.setBounds(180, 260, 70, 20);
 			btg2.addActionListener(this);
 			add(btg2);
 
 			infoArea.setBounds(500,20,400,500);
 			infoArea.setBackground(Color.LIGHT_GRAY);
-			add(infoArea);
+			infoArea.setEnabled(false);
 			infoArea.append(" NAME              |   ROLL        |   ADDRESS   |    GENDER   \n");
 			infoArea.append("\n");
 
+			selfIntro.setBounds(20,900,400,20);
 
-		}
+			// adding components to the panel
+            add(name);
+            add(nameT);
+            add(roll);
+            add(rollT);
+            add(add);
+//            add(AddT);
+            add(sp);
+			add(gen);
+            add(male);
+            add(female);
+            add(infoArea);
+            add(selfIntro);
+        }
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -118,6 +126,7 @@ public class StudentDetailsForm extends WriteToFile {
 					} catch (IOException ioException) {
 						ioException.printStackTrace();
 					}
+
 					nameT.setEditable(false);
 					nameT.setText("");
 					rollT.setEditable(false);
@@ -128,62 +137,51 @@ public class StudentDetailsForm extends WriteToFile {
 					male.setEnabled(false);
 					female.setEnabled(false);
 					btg1.setText("New");
-
 				}
 			}
 			else if(e.getSource() == btg2) {
 				System.exit(0);
 			}
-
-			
-		}
+        }
 
 		public void saveToFile() throws IOException {
 			WriteToFile data = new WriteToFile("./saved_details.txt", true);
+
 			// formating for showing on panel
 			String name = nameT.getText() + "   |  ";
 			String roll = rollT.getText() + "   |  ";
 			String address = AddT.getText() + "         |   ";
-			infoArea.setEditable(false);
+			infoArea.setEnabled(true);
 
-			WriteToFile readData = new WriteToFile("./saved_details.txt");
 			if(male.isSelected()){
 				String[] input = {name, roll, address , " male\n"};
 				for (String s : input) {
 					data.write(s);
 					infoArea.append(s);
 				}
-//				readData.readFromFile("./saved_details.txt");
-//				StringBuilder localBuffer = readData.stringBuffRead;
-//				System.out.println("From buffer "+ localBuffer);
-			}else {
+
+			}else if(female.isSelected()){
 				String[] input = {name, roll, address, "female\n"};
 				for (String s : input) {
 					data.write(s);
 					infoArea.append(s);
 				}
+				
 			}
-
 		}
-
 	}
-
 
 	public static class EducationDetails extends JPanel implements ItemListener {
 
+        String[] years = {"Options","First year", "Second year", "Third year", "Fourth year"};
+        JComboBox<String> cmb = new JComboBox<>(years);
 
-		JLabel info = new JLabel("Please choose a year of study and check respective subjects");
-		String[] years = {"Options","First year", "Second year", "Third year", "Fourth year"};
-		JComboBox<String> cmb = new JComboBox<String>(years);
-		// checkboxes for academic year topics
-			//first year
-		JCheckBox cbSub1 = new JCheckBox();
-		JCheckBox cbSub2 = new JCheckBox();
-		JCheckBox cbSub3 = new JCheckBox();
-			//second year
-		JCheckBox cb1sy = new JCheckBox("Molecular biology");
-		JCheckBox cb2sy = new JCheckBox("Java");
-		JCheckBox cb3sy = new JCheckBox("Electronics");
+        // checkboxes for academic year topics
+        JCheckBox cbSub1 = new JCheckBox();
+        JCheckBox cbSub2 = new JCheckBox();
+        JCheckBox cbSub3 = new JCheckBox();
+
+        JLabel info = new JLabel("Please choose a year of study and check respective subjects");
 
 		public EducationDetails(){
 			setLayout(null);
@@ -200,26 +198,19 @@ public class StudentDetailsForm extends WriteToFile {
 			add(cbSub2); cbSub2.setVisible(false);
 			add(cbSub3); cbSub3.setVisible(false);
 
-			// register the listener
+			// registering combo-box listener
 			cmb.addItemListener(this);
 
 			// add all components to the panel
 			add(info);
 			add(cmb);
-
-
 		}
 
 		@Override
 		public void itemStateChanged(ItemEvent e) {
-//			JPanel cbPanel = new JPanel();
-//			cbPanel.setBounds(30,200,200,200);
-//			cbPanel.setBackground(new Color(16, 50, 116));
-//			add(cbPanel);
 			if(e.getSource() == cmb){
 				if(e.getStateChange() == ItemEvent.SELECTED){
 					if (Objects.equals(cmb.getSelectedItem(), "First year")) {
-						System.out.println("First year was selected");
 						cbSub1.setVisible(true);
 						cbSub2.setVisible(true);
 						cbSub3.setVisible(true);
@@ -233,16 +224,16 @@ public class StudentDetailsForm extends WriteToFile {
 						cbSub2.setText("Java");
 						cbSub3.setText("Electronics");
 
-					}else if(Objects.equals((String)cmb.getSelectedItem(), "Third year")){
+					}else if(Objects.equals(cmb.getSelectedItem(), "Third year")){
 						cbSub1.setText("Genomics");
 						cbSub2.setText("ML/AI");
 						cbSub3.setText("Pharmacology");
 
-					}else if(Objects.equals((String)cmb.getSelectedItem(), "Fourth year")){
+					}else if(Objects.equals(cmb.getSelectedItem(), "Fourth year")){
 						cbSub1.setText("Robotics");
 						cbSub2.setText("Medical Imaging");
 						cbSub3.setText("Bio Transport");
-					}else if(Objects.equals((String)cmb.getSelectedItem(), "Options")){
+					}else if(Objects.equals(cmb.getSelectedItem(), "Options")){
 						cbSub1.setVisible(false);
 						cbSub2.setVisible(false);
 						cbSub3.setVisible(false);
@@ -251,25 +242,7 @@ public class StudentDetailsForm extends WriteToFile {
 			}
 		}
 	}
-
-	public static void main(String[] args) throws IOException {
-		new StudentDetailsForm();
-		
+	public static void main(String[] args) {
+		new StudentDetailsForm(); // spawn instance of GUI by calling the constructor
 	}
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- WriteToFile.java
+++ WriteToFile.java
@@ -1,13 +1,9 @@
 import java.io.*;
-import java.nio.CharBuffer;
-import java.util.Scanner;
 
 public class WriteToFile {
 
     String path;
     boolean append_to_file = false;
-    int stringBufferLength;
-    StringBuilder stringBuffRead = new StringBuilder();
 
     public WriteToFile(String file_path) {
         path = file_path;
@@ -15,56 +11,19 @@ public class WriteToFile {
     }
 
     public WriteToFile(String file_path, boolean append_value) {
+        // parameterized constructor with an option of appending to the file
         path = file_path;
         append_to_file = append_value;
     }
 
-    public WriteToFile() throws IOException {
-
-    }
-
     public void write(String textLine) throws IOException{
+        // This method writes the strings from any class to a file that is assigned to path variable
         FileWriter writer = new FileWriter(path, append_to_file);
         PrintWriter print_line = new PrintWriter(writer);
-
-//        print_line.printf("Name: %s \n ", textLine);
-//        print_line.printf("Roll. No.: %s \n ", textLine);
-//        print_line.printf("Address: %s \n ", textLine);
-//        print_line.printf("Gender: %s \n ", textLine);
         print_line.printf("%s ", textLine);
         print_line.close();
-
     }
 
-    public int readFromFile(String fileName) throws IOException {
-        File file = new File(fileName);
-        FileReader reader = new FileReader(file);
-        BufferedReader buffRead = new BufferedReader(reader);
-        String readLine;
-        while((readLine=buffRead.readLine())!=null){
-            stringBuffRead.append(readLine);
-            stringBuffRead.append("\n");
 
-        }
-        reader.close();
-        return stringBuffRead.length();
-    }
 }
 
-//
-//
-//    public void readFromFile() throws FileNotFoundException {
-//        File file = new File("./saved_details.txt");
-//        FileInputStream stream = new FileInputStream(file);
-//        Scanner scanLine = new Scanner(stream);
-//        while(scanLine.hasNextLine()){
-//            if(scanLine.toString().equals(infoArea.getText())){
-//                infoArea.setVisible(true);
-//                infoArea.append("\n");
-//                infoArea.append(scanLine.nextLine());
-//                System.out.println(scanLine.nextLine());
-//            }else{
-//                break;
-//            }
-//        }
-//    }
